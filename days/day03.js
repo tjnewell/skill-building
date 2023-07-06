64b36d2fc94100a605a4e518455e083acd43151a
TJ Newell <tj.newell@gmail.com>
2023-07-06 15:04:33 -0700

    day3-part2

--- days/day03.js
+++ days/day03.js
@@ -13,12 +13,12 @@ function sliceRucksacks(allRucksacks) {
    * @type {string[][]}
    */
   const splitRucksacks = [];
-  allRucksacks.map((rucksacks) => {
-    rucksacks.map((rucksack) => {
+  allRucksacks.forEach((rucksacks) => {
+    rucksacks.forEach((rucksack) => {
       const length = rucksack.length;
       const firstCompartment = rucksack.slice(0, length / 2);
       const secondCompartment = rucksack.slice(length / 2);
-      const newArray = new Array(firstCompartment, secondCompartment);
+      const newArray = [firstCompartment, secondCompartment];
       return splitRucksacks.push(newArray);
     });
   });
@@ -31,14 +31,13 @@ const allDividedRucksacks = sliceRucksacks(splitFile);
  * @param {string[]} rucksack
  * @returns {string[]}
  */
-function compareCompartments(rucksack) {
+function findDuplicates(rucksack) {
   let duplicates = [];
-  const splitSack = rucksack.flatMap((a) => a.split(", "));
-  const firstCompartment = splitSack[0].split("");
+  const firstCompartment = rucksack[0].split("");
   while (0 < firstCompartment.length) {
     const firstItem = firstCompartment.shift();
     if (firstItem) {
-      const isInSecondCompartment = splitSack[1].includes(firstItem);
+      const isInSecondCompartment = rucksack[1].includes(firstItem);
       if (isInSecondCompartment) {
         duplicates.push(firstItem);
         break;
@@ -49,7 +48,7 @@ function compareCompartments(rucksack) {
 }
 
 const duplicateItemInEachRucksack = allDividedRucksacks.flatMap((rucksack) => {
-  const listOfDuplicateItems = compareCompartments(rucksack);
+  const listOfDuplicateItems = findDuplicates(rucksack);
   return listOfDuplicateItems;
 });
 
@@ -76,11 +75,67 @@ function calculateTotalValue(values) {
   return total;
 }
 
-const combinedValues = duplicateItemInEachRucksack.map((item) => {
-  const value = calculateValue(item);
-  return value;
-});
+/**
+ *
+ * @param {string[]} items
+ * @returns {number[]}
+ */
+function combinedValues(items) {
+  const calculatedValue = items.map((item) => {
+    const value = calculateValue(item);
+    return value;
+  });
+  return calculatedValue;
+}
 
-console.log(calculateTotalValue(combinedValues));
+console.log(calculateTotalValue(combinedValues(duplicateItemInEachRucksack)));
 
-// Part 2 Wants to know what item, in a grouping of 3 rucksacks in order, are includes in the 3 rucksacks.
+/**
+ *
+ * @param {string[][]} allRucksacks
+ * @returns {string[][][]}
+ */
+const groupRucksacks = (allRucksacks) => {
+  const groupedRucksacks = [];
+  for (let i = 0; i < allRucksacks.length; i += 3) {
+    const rucksackGroup = allRucksacks.slice(i, i + 3);
+    groupedRucksacks.push(rucksackGroup);
+  }
+  return groupedRucksacks;
+};
+
+const groupedRucksacks = groupRucksacks(splitFile);
+
+/**
+ *
+ * @param {string[][][]} allGroupings
+ * @returns {string[]}
+ */
+
+function findDuplicatesInThreeSacks(allGroupings) {
+  /**
+   * @type {string[]}
+   */
+  let duplicates = [];
+  allGroupings.forEach((group) => {
+    const firstSackInGroup = group[0][0].split("");
+    while (0 < firstSackInGroup[0].length) {
+      const firstItem = firstSackInGroup.shift();
+      if (firstItem) {
+        const isInOtherSacks =
+          group[1][0].includes(firstItem) && group[2][0].includes(firstItem);
+        if (isInOtherSacks) {
+          duplicates.push(firstItem);
+          break;
+        }
+      }
+    }
+  });
+  return duplicates;
+}
+
+console.log(
+  calculateTotalValue(
+    combinedValues(findDuplicatesInThreeSacks(groupedRucksacks))
+  )
+);
